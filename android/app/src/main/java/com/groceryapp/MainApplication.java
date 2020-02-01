d70be6af9509a1bf4e788a2f4d2d57a36901be3b
arpit.garg <arpit.garg@impetus.co.in>
2020-02-02 02:05:08 +0530

    Added the Touch ID Authentication at LoginContainer.js

--- android/app/src/main/java/com/groceryapp/MainApplication.java
+++ android/app/src/main/java/com/groceryapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.rnfingerprint.FingerprintAuthPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
